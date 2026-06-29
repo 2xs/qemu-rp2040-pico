42d66ab9ae5a37241ceac30c24bc1ff87505c1b8
gilles grimaud <gilles.grimaud@univ-lille.fr>
2026-06-29 20:44:36 +0200

    hw/misc: add RP2040 ROSC register model

--- include/hw/arm/rp2040.h
+++ include/hw/arm/rp2040.h
@@ -17,6 +17,7 @@
 #include "hw/misc/rp2040_clocks.h"
 #include "hw/misc/rp2040_pll.h"
 #include "hw/misc/rp2040_resets.h"
+#include "hw/misc/rp2040_rosc.h"
 #include "hw/misc/rp2040_syscfg.h"
 #include "hw/misc/rp2040_sysinfo.h"
 #include "hw/misc/rp2040_tbman.h"
@@ -53,6 +54,7 @@ struct RP2040State {
     RP2040PllState pll_sys;
     RP2040PllState pll_usb;
     RP2040ResetsState resets;
+    RP2040RoscState rosc;
     RP2040SysCfgState syscfg;
     RP2040SysInfoState sysinfo;
     RP2040TbmanState tbman;
