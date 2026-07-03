b2a594f681820e1ade5614af386e41990431aec2
Gilles Grimaud <gilles.grimaud@univ-lille.fr>
2026-07-04 00:12:25 +0200

    hw/arm: add RP2040 SoC and Raspberry Pi Pico machine
    Add a raspi-pico machine built around a minimal RP2040 SoC model. The machine provides Cortex-M0+ cores, SRAM, XIP flash, UARTs, DMA, timers, watchdog, clocks/resets, SIO, GPIO banks, and the shallow peripheral surface needed by current bare-metal and Pico SDK tests.
    
    This continues the direction of Alex Bennee's 2022 RP2040/Pico RFC series, adapting the SoC skeleton, Pico board, memory map, and mask-ROM loading shape to current QEMU APIs. The RFC pipico.rom blob is intentionally not included; users can still provide an external RP2040 boot ROM with -bios.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Gilles Grimaud <gilles.grimaud@univ-lille.fr>

---
+++ hw/arm/raspi_pico.c
@@ -0,0 +1,157 @@
+/*
+ * Raspberry Pi Pico machine
+ *
+ * Copyright (c) 2021 Linaro Ltd
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/units.h"
+#include "qapi/error.h"
+#include "hw/arm/boot.h"
+#include "hw/arm/machines-qom.h"
+#include "hw/arm/rp2040.h"
+#include "hw/core/boards.h"
+#include "hw/core/qdev-properties.h"
+#include "system/address-spaces.h"
+#include "system/system.h"
+#include "qom/object.h"
+
+#define TYPE_RASPI_PICO_MACHINE MACHINE_TYPE_NAME("raspi-pico")
+OBJECT_DECLARE_SIMPLE_TYPE(RaspiPicoMachineState, RASPI_PICO_MACHINE)
+
+struct RaspiPicoMachineState {
+    MachineState parent_obj;
+
+    RP2040State soc;
+    char *flash_file;
+    bool strict_uart_pins;
+};
+
+static char *raspi_pico_get_flash_file(Object *obj, Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    return g_strdup(s->flash_file ?: "");
+}
+
+static void raspi_pico_set_flash_file(Object *obj, const char *value,
+                                      Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    g_free(s->flash_file);
+    s->flash_file = g_strdup(value);
+}
+
+static void raspi_pico_init(MachineState *machine)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(machine);
+    MemoryRegion *system_memory = get_system_memory();
+
+    object_initialize_child(OBJECT(machine), "soc", &s->soc, TYPE_RP2040);
+    qdev_prop_set_chr(DEVICE(&s->soc), "serial0", serial_hd(0));
+    qdev_prop_set_chr(DEVICE(&s->soc), "serial1", serial_hd(1));
+    qdev_prop_set_bit(DEVICE(&s->soc), "strict-uart-pins",
+                      s->strict_uart_pins);
+    /*
+     * BOOTSEL is not pressed by default on a Pico board, so the mask ROM sees
+     * the QSPI SS input deasserted and tries to boot from external flash.
+     */
+    qdev_prop_set_uint32(DEVICE(&s->soc.sio), "gpio-hi-in", 1u << 1);
+    if (s->flash_file) {
+        qdev_prop_set_string(DEVICE(&s->soc.xip), "flash-file",
+                             s->flash_file);
+    }
+    if (machine->firmware) {
+        qdev_prop_set_string(DEVICE(&s->soc), "bootrom-file",
+                             machine->firmware);
+    }
+    object_property_set_link(OBJECT(&s->soc), "memory",
+                             OBJECT(system_memory), &error_fatal);
+
+    sysbus_realize(SYS_BUS_DEVICE(&s->soc), &error_fatal);
+
+    /*
+     * For now, -kernel images are loaded directly into the XIP window.
+     * rp2040_xip_load_image() accepts ELF images first, then raw images.
+     */
+    rp2040_xip_load_image(&s->soc.xip, machine->kernel_filename,
+                          &error_fatal);
+    armv7m_load_kernel(s->soc.armv7m[0].cpu, NULL, RP2040_XIP_BASE, 2 * MiB);
+    rp2040_xip_set_writable(&s->soc.xip, false);
+}
+
+static void raspi_pico_machine_finalize(Object *obj)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    g_free(s->flash_file);
+}
+
+static bool raspi_pico_get_strict_uart_pins(Object *obj, Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    return s->strict_uart_pins;
+}
+
+static void raspi_pico_set_strict_uart_pins(Object *obj, bool value,
+                                            Error **errp)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    s->strict_uart_pins = value;
+}
+
+static void raspi_pico_machine_initfn(Object *obj)
+{
+    RaspiPicoMachineState *s = RASPI_PICO_MACHINE(obj);
+
+    s->strict_uart_pins = true;
+}
+
+static void raspi_pico_machine_class_init(ObjectClass *oc, const void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(oc);
+
+    mc->desc = "Raspberry Pi Pico (Cortex-M0+)";
+    mc->init = raspi_pico_init;
+    mc->default_cpus = RP2040_NUM_CORES;
+    mc->min_cpus = RP2040_NUM_CORES;
+    mc->max_cpus = RP2040_NUM_CORES;
+    mc->no_parallel = 1;
+    mc->no_floppy = 1;
+    mc->no_cdrom = 1;
+
+    object_class_property_add_str(oc, "flash-file",
+                                  raspi_pico_get_flash_file,
+                                  raspi_pico_set_flash_file);
+    object_class_property_set_description(oc, "flash-file",
+                                          "Load initial XIP flash contents "
+                                          "from a raw host file");
+    object_class_property_add_bool(oc, "strict-uart-pins",
+                                   raspi_pico_get_strict_uart_pins,
+                                   raspi_pico_set_strict_uart_pins);
+    object_class_property_set_description(oc, "strict-uart-pins",
+                                          "Require GPIO0/GPIO1 IO_BANK0 "
+                                          "FUNCSEL=UART before UART0 reaches "
+                                          "the host serial backend");
+}
+
+static const TypeInfo raspi_pico_machine_info = {
+    .name = TYPE_RASPI_PICO_MACHINE,
+    .parent = TYPE_MACHINE,
+    .instance_size = sizeof(RaspiPicoMachineState),
+    .instance_init = raspi_pico_machine_initfn,
+    .class_init = raspi_pico_machine_class_init,
+    .instance_finalize = raspi_pico_machine_finalize,
+    .interfaces = arm_machine_interfaces,
+};
+
+static void raspi_pico_machine_init(void)
+{
+    type_register_static(&raspi_pico_machine_info);
+}
+type_init(raspi_pico_machine_init)
